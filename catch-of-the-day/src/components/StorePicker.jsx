b68b21744484eda1b5a9a54834573eac0f7c17a3
Herve Desrosiers <hdesrosiers@stingray.com>
2018-08-17 07:53:40 -0400

    created ref and event handler as property arrow function 11. EVENTS, REFS AND THIS BINDING

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
-  state = {};
+  myInput = React.createRef(); //create a ref to access the <input/> element
+  //set method as a property to bind this on StorePicker
+  goToStore = e => {
+    //pass event argument
+    //1. stop the form from submitting
+    e.preventDefault();
+    //2. get the text from the input (with a ref)
+    const storeName = this.myInput.value;
+    console.log(storeName);
+    //3. change page to /store/:captured_input
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          ref={this.myInput}
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
