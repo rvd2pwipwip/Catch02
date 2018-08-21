4e95f72517d7a944859469d7bf2c6102fec13849
Herve Desrosiers <hdesrosiers@stingray.com>
2018-08-21 08:17:21 -0400

    Load sample fishes and map their data to a <ul/> below the <Header />

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
   state = {
@@ -16,14 +18,27 @@ class App extends Component {
     //3.set new fishes object to state
     this.setState({ fishes });
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(fish => (
+              <Fish key={fish} details={this.state.fishes[fish]}>
+                {fish}
+              </Fish>
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
---
+++ catch-of-the-day/src/components/Fish.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Fish extends Component {
+  state = {};
+  render() {
+    return <li className="single-fish">Fish</li>;
+  }
+}
+
+export default Fish;
