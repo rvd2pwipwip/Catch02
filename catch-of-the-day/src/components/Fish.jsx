471c4cbc0b1f33a9cb0d80f888a29924b992e554
Herve Desrosiers <hdesrosiers@stingray.com>
2018-08-21 12:41:44 -0400

    formatted Fiish template to display fish data

--- catch-of-the-day/src/components/Fish.jsx
+++ catch-of-the-day/src/components/Fish.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { formatPrice } from "../helpers";
 
 class Fish extends Component {
   state = {};
   render() {
-    return <li className="single-fish">Fish</li>;
+    const { image, name, desc, price, status } = this.props.details;
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add to Order</button>
+      </li>
+    );
   }
 }
 
