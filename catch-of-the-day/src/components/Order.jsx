2561f00d0fc2f34d13441569a1cc4719093558d3
Herve Desrosiers <hdesrosiers@stingray.com>
2018-08-27 08:09:59 -0400

    Order component render total with reduce + map to <li/>

--- catch-of-the-day/src/components/Order.jsx
+++ catch-of-the-day/src/components/Order.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends Component {
-  state = {};
   render() {
-    return <div className="order">Order</div>;
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul>
+          {orderIds.map(key => (
+            <li>{key}</li>
+          ))}
+        </ul>
+        <div className="total">
+          Total:
+          <strong>{` ${formatPrice(total)}`}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
